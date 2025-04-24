f5523ee26dd5b2df2d1cfd8b8988d1e83c2b0530
Paulo Victor <vitorcanalle@gmail.com>
2025-04-24 16:03:20 -0300

    feat:  Iniciando routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Pages
+import Home from './routes/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
