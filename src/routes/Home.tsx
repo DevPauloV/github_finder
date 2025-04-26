978eb4f495f60e05cea20d17aa08f2f4d2dfd108
Paulo Victor <vitorcanalle@gmail.com>
2025-04-26 00:22:05 -0300

    feat: adiciona componente Search e tipagem inicial de usuário

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,10 @@
 import Search from "../components/Search";
+import { useState } from "react";
+import { UserProps } from "../types/user";
 
 const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
   return (
     <div>
       <Search />
