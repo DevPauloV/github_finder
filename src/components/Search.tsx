0168df9f871b237ec830cf700d76f21f6f015db0
Paulo Victor <vitorcanalle@gmail.com>
2025-04-27 13:13:45 -0300

    feat: implementa função loadUser na Home e integra busca de usuário no componente Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,23 @@
+type SearchProps = {
+    loadUser: (username: string) => Promise<void>;
+};
+
+import { useState } from 'react';
 
 import { BsSearch } from 'react-icons/bs'
 
-const Search = () => {
+const Search = ({loadUser}: SearchProps) => {
+    const [username, setUsername] = useState("");
+
     return (
         <div>
             <h2>Busque por um usuário:</h2>
             <p>Conheça seus melhores repositórios</p>
             <div>
-                <input type="text" placeholder='Digite o nome do usuário' />
-                <button aria-label="Buscar usuário">
+                <input type="text" placeholder="Digite o nome do usuário"
+                onChange={(e) => setUsername(e.target.value)} />
+                
+                <button aria-label="Buscar usuário" onClick={() => loadUser(username)}>
                     <BsSearch />
                 </button>
             </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,9 +5,18 @@ import { UserProps } from "../types/user";
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
 
+  const loadUser = async (username: string) => {
+
+    const res = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await res.json();
+
+    console.log(data);
+  }
+
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
     </div>
   );
 };
